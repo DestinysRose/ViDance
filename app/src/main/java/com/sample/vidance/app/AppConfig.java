214f075e30da51ee2b798be672b76918b2e1a429
DestinysRose <Vixen_Minamino@hotmail.com>
2017-04-13 05:25:44 +0800

    Merged with Danil's connection files. Unable to connect.

---
+++ app/src/main/java/com/sample/vidance/app/AppConfig.java
@@ -0,0 +1,17 @@
+package com.sample.vidance.app;
+
+/**
+ * Created by Danil on 27.03.2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://thevidance.com/alogin.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://thevidance.com/register.php";
+
+    // Server user register url
+    public static String URL_BEH_TEST = "http://thevidance.com/store_b.php";
+
+}
---
+++ app/src/main/java/com/sample/vidance/helper/BehaviourHandler.java
@@ -0,0 +1,18 @@
+package com.sample.vidance.helper;
+
+/**
+ * Created by Danil on 29.03.2017.
+ */
+
+public class BehaviourHandler {
+    //JSON URL
+    public static final String DATA_URL = "http://192.168.43.98/android_login/behaviour.php";
+
+    //Tags used in the JSON String
+    public static final String TAG_SEVER = "severity";
+    public static final String TAG_BNAME = "bName";
+    public static final String TAG_BID = "b_id";
+
+    //JSON array name
+    public static final String JSON_ARRAY = "result";
+}
